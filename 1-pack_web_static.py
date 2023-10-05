4805d33dc62378586d2b79435cd50cc4153f8307
cartermusee <cartermusee@gmail.com>
2023-10-05 13:01:35 -0700

    fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """
+     Fabric script that generates a .tgz archive
+    """
+    local("mkdir -p version")
+
+    now = datetime.now()
+    arch = "web_static_{}{}{}{}{}{}.tgz".\
+        format(now.year,
+               now.month,
+               now.day, now.hour, now.minute, now.second)
+    result = local("tar -cvzf version/{} web_static".format(arch))
+    if result.succeeded:
+        return "version/{}".format(arch)
+    else:
+        return None
