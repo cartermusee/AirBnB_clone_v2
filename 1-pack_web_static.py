5c2c07cb22f9887cb8fd9ec6fbf9df6246db5ce5
cartermusee <cartermusee@gmail.com>
2023-10-05 21:50:04 -0700

    webstatic

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,13 +7,13 @@ def do_pack():
     """
      Fabric script that generates a .tgz archive
     """
-    local("mkdir -p version")
+    local("mkdir -p versions")
 
     now = datetime.now()
     fm_date = now.strftime("%Y%m%d%H%M%S")
-    result = local("tar -czvf version/web_static_{}.tgz web_static".\
+    result = local("tar -czvf versions/web_static_{}.tgz web_static".\
             format(fm_date))
     if result.succeeded:
-        return "version/web_static_{}.tgz".format(fm_date)
+        return "versions/web_static_{}.tgz".format(fm_date)
     else:
         return None
