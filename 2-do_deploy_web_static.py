f5e73053518b01c88a7d13d8433d55b0cbab662e
cartermusee <cartermusee@gmail.com>
2023-10-07 05:39:29 -0700

    mv

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,14 +16,19 @@ def do_deploy(archive_path):
             return False
         put(archive_path, "/tmp/")
         filename = os.path.basename(archive_path.split('.')[0])
-        folder = run("sudo mkdir -p /data/web_static/releases/{}/".
-                     format(filename))
-        run('sudo tar -xzvf /tmp/{} -C /data/web_static/releases/{}/'.
+        run("sudo mkdir -p /data/web_static/releases/{}/".
+            format(filename))
+        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.
             format(os.path.basename(archive_path), filename))
         run("sudo rm  /tmp/{}".format(os.path.basename(archive_path)))
-        run("sudo rm -f /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /\
-                data/web_static/current".format(filename))
+        run("mv /data/web_static/releases/{}/web_static/* \
+/data/web_static/releases/{}/".
+            format(filename, filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".
+            format(filename))
+        run("sudo rm -rf data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ \
+/data/web_static/current".format(filename))
         return True
     except Exception as e:
         return False
