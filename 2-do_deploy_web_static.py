c6577c91ba04c69c7fcf63d3a038738d0f8e937b
cartermusee <cartermusee@gmail.com>
2023-10-07 05:03:45 -0700

    deplay

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-
+"""to deplaoy"""
 from fabric.api import *
 import os
 from datetime import datetime
 
 
-env.hosts = ['52.87.222.165', '54.237.12.4']
 env.user = 'ubuntu'
+env.hosts = ['52.87.222.165', '54.237.12.4']
 env.key_filename = "~/.ssh/id_rsa"
-def do_deploy(archive_path):
-    """
-     Fabric script that generates a .tgz archive
-    """
 
+
+def do_deploy(archive_path):
     try:
         if not os.path.exists(archive_path):
             return False
-
         put(archive_path, "/tmp/")
-
-        arch_file = os.path.basename(archive_path)
-        folder = "/data/web_static/releases/.{}".\
-                 format(arch_file.split('.')[0])
-        run("mkdir -p {}".format(folder))
-        run("tar -xzf /tmp/{} -C {}".format(arch_file, folder))
-        run("rm -r /tmp/{}".format(arch_file))
-
-        run("rm -f /data/web_static/current")
-        run('ln -s {} /data/web_static/current'.format(folder))
+        filename = os.path.basename(archive_path.split('.')[0])
+        folder = run("sudo mkdir -p /data/web_static/releases/{}/".
+                     format(filename))
+        run('sudo tar -xzvf /tmp/{} -C /data/web_static/releases/{}/'.
+            format(os.path.basename(archive_path), filename))
+        run("sudo rm  /tmp/{}".format(os.path.basename(archive_path)))
+        run("sudo rm -f /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ /\
+                data/web_static/current".format(filename))
         return True
     except Exception as e:
         return False
-    return True
