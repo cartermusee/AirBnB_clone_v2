bb08b465edab949a5a459478e8b07bdaea50d15c
cartermusee <cartermusee@gmail.com>
2023-10-08 23:35:22 -0700

    solved mv

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -20,6 +20,10 @@ def do_pack():
 
 
 def do_deploy(archive_path):
+    """finction deploy
+    arg:
+        archive_path:the path
+    """
     try:
         if not os.path.exists(archive_path):
             return False
@@ -30,17 +34,14 @@ def do_deploy(archive_path):
         run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.
             format(os.path.basename(archive_path), filename))
         run("sudo rm  /tmp/{}".format(os.path.basename(archive_path)))
-        run("sudo mv /data/web_static/releases/{}/web_static/* \
-/data/web_static/releases/{}/".
-            format(filename, filename))
-        run("sudo rm -r /data/web_static/releases/{}/web_static".
+        run("sudo mv  /data/web_static/releases/{}/web_static/* \
+/data/web_static/releases/{}/".format(filename, filename))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".
             format(filename))
         run("sudo rm -rf data/web_static/current")
         run("sudo ln -s /data/web_static/releases/{}/ \
 /data/web_static/current".format(filename))
         return True
-    except Exception as e:
-        return False
 
 
 def deploy():
