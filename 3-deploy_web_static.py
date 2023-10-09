dba4e90c6eb950a7b6b39df8c011352d375a71a5
cartermusee <cartermusee@gmail.com>
2023-10-08 23:42:24 -0700

    solved mv

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """module for deploy"""
+
+
 from fabric.api import *
 from datetime import datetime
 
@@ -8,15 +10,15 @@ def do_pack():
     """
      Fabric script that generates a .tgz archive
     """
-    local("mkdir -p versions")
+    try:
+        local("mkdir -p versions")
 
-    now = datetime.now()
-    fm_date = now.strftime("%Y%m%d%H%M%S")
-    result = local("tar -czvf versions/web_static_{}.tgz web_static".
-                   format(fm_date))
-    if result.succeeded:
+        now = datetime.now()
+        fm_date = now.strftime("%Y%m%d%H%M%S")
+        result = local("tar -czvf versions/web_static_{}.tgz web_static".
+                       format(fm_date))
         return "versions/web_static_{}.tgz".format(fm_date)
-    else:
+    except Exception as e:
         return None
 
 
