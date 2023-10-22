340d21e4e88a87d9cf62beb9c79ae5a90a3e6bd2
cartermusee <cartermusee@gmail.com>
2023-10-22 14:50:04 -0700

    flask

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_st(exception):
+    storage.close()
+
+
+
+@app.route("/states_list", strict_slashes=False)
+def states():
+    states = storage.all("State")
+    sortedd = sorted(states.values(), key=lambda state: state.name)
+    return render_template("7-states_list.html", states=sortedd)
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
