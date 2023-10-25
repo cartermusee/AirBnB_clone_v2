e7975e3028c176a909f65ffcb550f3fc35f03387
cartermusee <cartermusee@gmail.com>
2023-10-25 06:23:33 -0700

    hnbn

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -9,16 +9,17 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_st(self):
+def close_st(exc):
     storage.close()
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb_filter():
     states = storage.all(State)
     amenities = storage.all("Amenity")
-    return render_template("10-hbnb_filters.html", states=states,
-                           amenities=amenities)
+    places = storage.all("Place")
+    return render_template("100-hbnb.html", states=states,
+                           amenities=amenities, places=places)
 
 
 if __name__ == "__main__":
